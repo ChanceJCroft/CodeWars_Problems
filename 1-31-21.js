b801585b703ac23c007ae2f5f4ab5305a190dbbb
Chance Croft <66639666+ChanceJCroft@users.noreply.github.com>
2021-01-31 16:39:13 -0600

    Add files via upload

--- 1-31-21.js
+++ 1-31-21.js
@@ -75,3 +75,90 @@ function nbYear(p0, percent, aug, p) {
   
     return counter;
 }
+
+
+
+/* Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0] */
+
+var moveZeros = function (arr) {
+  let newArr = [];
+  let zeroCounter = 0;
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] === 0) {
+      zeroCounter += 1;
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  
+  for(let i = 0; i < zeroCounter; i++) {
+      newArr.push(0);
+  }
+  
+  return newArr;
+}
+
+/* Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
+
+The left side letters and their power:
+
+ w - 4
+ p - 3
+ b - 2
+ s - 1
+The right side letters and their power:
+
+ m - 4
+ q - 3
+ d - 2
+ z - 1
+The other letters don't have power and are only victims.
+
+Example
+alphabetWar("z");        //=> Right side wins!
+alphabetWar("zdqmwpbs"); //=> Let's fight again!
+alphabetWar("zzzzs");    //=> Right side wins!
+alphabetWar("wwwwwwz");  //=> Left side wins! */
+
+function alphabetWar(fight)
+{
+  let leftSide = 0;
+  let rightSide = 0;
+  const strArr = fight.split('');
+  for(let i = 0; i < strArr.length; i++) {
+      if(strArr[i] == 'w') {
+        leftSide += 4;
+      }
+      if(strArr[i] == 'p') {
+        leftSide += 3;
+      }
+      if(strArr[i] == 'b') {
+        leftSide += 2;
+      }
+      if(strArr[i] == 's') {
+        leftSide += 1;
+      }
+    if(strArr[i] == 'm') {
+        rightSide += 4;
+      }
+    if(strArr[i] == 'q') {
+        rightSide += 3;
+      }
+    if(strArr[i] == 'd') {
+        rightSide += 2;
+      }
+    if(strArr[i] == 'z') {
+        rightSide += 1;
+      }
+  }
+  
+  if(leftSide > rightSide) {
+    return "Left side wins!"
+  } else if (rightSide > leftSide) {
+    return "Right side wins!"
+  } else {
+    return "Let's fight again!";
+  }
+}
